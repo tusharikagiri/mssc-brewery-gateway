1f7bd51437935171edfa93b2e84d1875f2da314b
tusharikagiri <126240171+tusharikagiri@users.noreply.github.com>
2025-01-23 16:10:42 +0530

    Added local beer services routes

---
+++ src/main/java/com/sfg/gateway/config/LocalRouteConfig.java
@@ -0,0 +1,21 @@
+package com.sfg.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalRouteConfig {
+	@Bean
+	public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/api/v1/beers", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+						.uri("http://localhost:8080"))
+				.route(r -> r.path("/api/v1/customers/**")
+						.uri("http://localhost:8081"))
+				.route(r -> r.path("/api/v1/beer/*/inventory")
+						.uri("http://localhost:8082"))
+				.build();
+	}
+}
