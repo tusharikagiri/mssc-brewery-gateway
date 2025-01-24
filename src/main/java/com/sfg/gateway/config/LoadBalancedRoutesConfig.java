4543e2dd04ced0bad48cca60e0b349b86dc6c7d9
tusharikagiri <126240171+tusharikagiri@users.noreply.github.com>
2025-01-24 15:31:43 +0530

    Added circuit breaker route

--- src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -12,12 +12,18 @@ public class LoadBalancedRoutesConfig {
 	@Bean
 	public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(r -> r.path("/api/v1/beers", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-						.uri("lb://beer-service"))
-				.route(r -> r.path("/api/v1/customers/**")
+				.route("beer-service", r -> r.path("/api/v1/beers", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+						.uri("lb://beer-service")
+						)
+				.route("order-service", r -> r.path("/api/v1/customers/**")
 						.uri("lb://order-service"))
-				.route(r -> r.path("/api/v1/beer/*/inventory")
+				.route("inventory-service", r -> r.path("/api/v1/beer/*/inventory")
+						.filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+								.setFallbackUri("forward:/inventory-failover")
+								.setRouteId("inv-failover")))
 						.uri("lb://inventory-service"))
+				.route("inventory-failover-service", r -> r.path("/inventory-failover/**")
+						.uri("lb://inventory-failover"))
 				.build();
 	}
 }
